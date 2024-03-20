f31bebb77c27e8e4ed1d64f09c5ff73533e38fd7
Teqnify <cammanb007@gmail.com>
2024-03-20 16:01:50 -0500

    NON-WORKING (Fleshed out functions)

--- src/SplayTester.java
+++ src/SplayTester.java
@@ -5,19 +5,68 @@
  * Began writing code on 3/7/2024
  * 
  * My personal conventions are to put instance variables at the top of classes, followed by constructors and then other methods.
- * I also include the opening curly brace on the same line, sorry if that feels quite unreadable
+ * I also include the opening curly brace on the same line, sorry if that feels odd
  * Old habits die hard, and if you program a different way I understand.
 */
 import java.util.Scanner;
 
 public class SplayTester {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
+        // Init scanner and tree
         Scanner s = new Scanner(System.in);
         System.out.print("Enter an integer n to initialize the splay tree: ");
         int n = s.nextInt();
+        s.nextLine(); // Clear buffer
         SplayTree tree = new SplayTree(n);
         tree.preOrderPrint();
 
+        // Init used constants outside loop
+        String menuPromptString = "Enter a letter to select what to do with your splay tree.\n(a) Search for an integer\n(b) Insert a new integer of choice\n(c) Delete an integer of choice\n(*) Enter anything else to print the tree and quit";
+        String terminalPromptEmulator = "> ";
+        String searchPrompt = "Enter the integer you'd like to search for (touch and splay): ";
+        String insertPrompt = "Enter the integer you'd like to insert: ";
+        String deletePrompt = "Enter the integer you'd like to delete: ";
+        boolean shouldContinueRunningPromptLoop = true;
+
+        // Prompt loop
+        while(shouldContinueRunningPromptLoop){
+            System.out.println(menuPromptString);
+            System.out.print(terminalPromptEmulator);
+            String userChoice = s.nextLine();
+            char option = userChoice.charAt(0);
+            switch(option){
+                case 'a':{
+                    // Search
+                    System.out.print(searchPrompt);
+                    int inputInt = s.nextInt();
+                    s.nextLine(); // Clear buffer
+                    tree.search(inputInt);
+                    break;
+                }
+                case 'b':{
+                    // Insert
+                    System.out.print(insertPrompt);
+                    int inputInt = s.nextInt();
+                    s.nextLine(); // Clear buffer
+                    tree.insert(inputInt);
+                    break;
+                }
+                case 'c':{
+                    // Delete
+                    System.out.print(deletePrompt);
+                    int inputInt = s.nextInt();
+                    s.nextLine(); // Clear buffer
+                    tree.delete(inputInt);
+                    break;
+                }
+                default:{
+                    // Exit
+                    shouldContinueRunningPromptLoop = false;
+                    break;
+                }
+            }
+            tree.preOrderPrint();
+        }
         s.close();
     }
 }
--- src/SplayTree.java
+++ src/SplayTree.java
@@ -10,14 +10,17 @@ public class SplayTree {
     private class Node {
         // To properly implement any Tree, we'll need a Node class.
         // Since a splay tree is a special binary tree, the Node class includes left and right pointers.
+        // The splay tree node also includes a parent field for rotations.
         // This class literally speaks for itself so I'm not commenting any of the code.
         // You've read java before :)
         private int element;
+        private Node parent;
         private Node left;
         private Node right;
 
         protected Node(int element, Node left, Node right){
             this.element = element;
+            this.parent = null;
             this.left = left;
             this.right = right;
         }
@@ -33,26 +36,27 @@ public class SplayTree {
         Node currentNode = root;
         for(int i = 2; i < n+1; i++){
             currentNode.right = new Node(i, null, null);
+            currentNode.right.parent = currentNode;
             currentNode = currentNode.right;
         }
     }
 
+    // Alternative constructor, for use in deletion
+    public SplayTree(){
+        this.root = null;
+    }
+
     // Standard BST search, since I want to use this for convenience in other functions
     // Token helper function
-    private Node findNode(int k){
-        Node currentNode = root;
-        while(true){
-            if(k < currentNode.getElement()){
-                currentNode = currentNode.left;
-            } else if(k > currentNode.getElement()){
-                currentNode = currentNode.right;
-            } else if(k == currentNode.getElement()){
-                return currentNode;
-            } else {
-                 // I'm choosing the arbitrary convention of not alerting when the node isn't found. Entirely my own choice, I just wanted to keep the function pure.
-                 // I don't want to print an error message.
-                return null;
-            }
+    private Node findNode(Node node, int k){
+        if(k == node.getElement()){
+            return node;
+        } else if(k < node.getElement()){
+            return findNode(node.left, k);
+        } else if(k > node.getElement()){
+            return findNode(node.right, k);
+        } else{
+            return null;
         }
     }
 
@@ -91,13 +95,54 @@ public class SplayTree {
         }
     }
 
+    private void leftRotate(Node a){
+        Node b = a.right;
+        a.right = b.left;
+        if(b.left != null){
+            b.left.parent = a;
+        }
+        b.parent = a.parent;
+        if(a.parent == null){
+            // passed parent must be root to get here
+            this.root = a;
+        } else if(a == a.parent.left){
+            // passed parent is a left child
+            a.parent.left = b;
+        } else{
+            // passed parent is a right child
+            a.parent.right = b;
+        }
+        b.left = a;
+        a.parent = b;
+    }
+
+    private void rightRotate(Node a){
+        Node b = a.left;
+        a.left = b.right;
+        if(b.right != null){
+            b.right.parent = a;
+        }
+        b.parent = a.parent;
+        if(a.parent == null){
+            // passed parent must be root to get here
+            this.root = b;
+        } else if(a == a.parent.right){
+            // passed parent is a right child
+            a.parent.right = b;
+        } else{
+            a.parent.left = b;
+        }
+        b.right = a;
+        a.parent = b;
+    }
+
     // Standard binary tree search with a splay at the end
     // Returns a bool indicating whether the requested value was present within the tree (implying splay performed if true returned)
     // Effective wrapper over findNode(int k) with null case handling.
     public boolean search(int k){
-        Node toSplay = findNode(k);
+        Node toSplay = findNode(root, k);
         if(toSplay != null){
-            splay(root, toSplay);
+            splay(toSplay);
             return true;
         }
         return false;
@@ -109,24 +154,46 @@ public class SplayTree {
     // Delete it and handle child cases with proper state (select correct new root)
     // Returns a bool indicating whether the requested value was present within the tree
     public boolean delete(int k){
-        Node toDelete = findNode(k);
+        Node toDelete = findNode(root, k);
         if(toDelete == null){
             return false;
         }
-        splay(root, toDelete);
+        splay(toDelete);
         Node leftChild = toDelete.left;
         Node rightChild = toDelete.right;
         if(leftChild == null){
-            root = rightChild;
+            this.root = rightChild;
             return true;
         }
-        Node maxLeftSubtree = leftChild;
-        while(maxLeftSubtree.right != null){
-            maxLeftSubtree = maxLeftSubtree.right;
+
+        SplayTree leftSubtree = new SplayTree();
+        leftSubtree.root = root.left;
+        if (leftSubtree.root != null) {
+            leftSubtree.root.parent = null;
         }
-        splay(leftChild, maxLeftSubtree);
-        // make right the rightchild of left
-        // make left the root
+
+        SplayTree rightSubtree = new SplayTree();
+        rightSubtree.root = this.root.right;
+        if(rightSubtree.root != null){
+            rightSubtree.root.parent = null;
+        }
+
+        if(leftSubtree.root != null){
+            Node maxLeftSubtree = leftChild;
+            while(maxLeftSubtree.right != null){
+                maxLeftSubtree = maxLeftSubtree.right;
+            }
+            // Bring maximum of left subtree to root of left subtree (it will have no right child)
+            leftSubtree.splay(maxLeftSubtree);
+            // Set right child of new root to be the right subtree's root
+            leftSubtree.root.right = rightSubtree.root;
+            // Set this newly made root to be root of overall tree
+            this.root = leftSubtree.root;
+        } else{
+            // No reason to worry about left subtree
+            this.root = rightSubtree.root;
+        }
+
         return true;
     }
 
@@ -135,39 +202,88 @@ public class SplayTree {
     // Splay newly inserted node to root
     // Returns a bool indicating whether a splay occured
     public boolean insert(int k){
-        Node parent = root;
-        Node child;
-        while(true){
-            if(k < parent.getElement()){
-                if(parent.left == null){
-                    parent.left = new Node(k, null, null);
-                    child = parent.left;
-                    break;
-                }
-            } else if(k > parent.getElement()){
-                if(parent.left == null){
-                    parent.right = new Node(k, null, null);
-                    child = parent.right;
-                    break;
-                }
-            } else {
-                // parent must be equal to k, no duplicates allowed
+        Node child = new Node(k, null, null);
+        Node currentNode = root;
+        Node parent = null;
+
+        // Find parent to assign to new node
+        while(currentNode != null){
+            parent = currentNode;
+            if(k < currentNode.getElement()){
+                // Left traversal
+                currentNode = parent.left;
+            } else if(k > currentNode.getElement()) {
+                // Right traversal
+                currentNode = parent.right;
+            } else{
+                // Duplicate arrived at
                 return false;
             }
         }
-        splay(root, child);
+
+        // Assign that parent
+        child.parent = parent;
+
+        // Correctly assign child to parent
+        if(parent == null){
+            // We just added the root of the tree in this case
+            root = child;
+        } else if(k < parent.getElement()){
+            parent.left = child;
+        } else if(k > parent.getElement()) {
+            parent.right = child;
+        } else{
+            // Duplicate arrived at
+            return false;
+        }
+
+        splay(child);
         return true;
     }
 
     // The meat and potatoes of this project: actually splaying nodes.
     // Chose to allow selecting the effective root so I can splay subtrees (as in deletion algo)
-    public void splay(Node root, Node nodeToSplay){
-        return;
+    // Also chose to rely on helper methods to keep this code readable
+    public void splay(Node toSplay){
+        while(toSplay.parent != null){
+            // Handle rotations case-by-case
+            if(toSplay.parent == root){
+                if(toSplay == root.left){
+                    // Left case
+                    rightRotate(root);
+                } else{
+                    // Right case
+                    leftRotate(root);
+                }
+            } else {
+                Node parent = toSplay.parent;
+                Node grandparent = parent.parent;
+
+                if(parent.left == toSplay && grandparent.left == parent){
+                    // Left-left case
+                    rightRotate(grandparent);
+                    rightRotate(parent);
+                } else if(parent.right == toSplay && grandparent.right == parent){
+                    // Right-right case
+                    leftRotate(grandparent);
+                    leftRotate(parent);
+                } else if(parent.right == toSplay && grandparent.left == parent){
+                    // Left-right case
+                    leftRotate(parent);
+                    rightRotate(grandparent);
+                } else if(parent.left == toSplay && grandparent.right == parent){
+                    // Right-left case
+                    rightRotate(parent);
+                    leftRotate(grandparent);
+                }
+            }
+        }
     }
 
     // public-facing version of preOrderPrint to keep address of root private.
     public void preOrderPrint(){
         recPreOrderPrint(this.root, 0);
+        System.out.println();
         return;
     }
 }
